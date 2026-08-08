79aecb41736287297f0a1e83fee7b7d710f61d9c
Amit Pant <amitpantprofessionalacc@gmail.com>
2026-08-08 23:46:57 +0530

    Prepare project for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 const dns = require("dns");
-
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
-
 
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
-dotenv.config({ path: "./server/.env" });
+dotenv.config();
 
 const app = express();
 
@@ -27,8 +25,4 @@ const contactRoutes = require("./routes/contactRoutes");
 app.use("/api/projects", projectRoutes);
 app.use("/api/contact", contactRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
